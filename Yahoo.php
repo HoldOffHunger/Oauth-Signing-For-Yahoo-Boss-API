22cba7863d4914250962688b2e02ca7110d031fb
HoldOffHunger <holdoffhunger@gmail.com>
2017-04-17 20:26:52 -0400

    Create Yahoo.php
    This file provides the code for the oauth-signing and the demo for calling the oauth-signing functions for Yahoo's BOSS API (deprecated).

---
+++ Yahoo.php
@@ -0,0 +1,62 @@
+<?php
+	
+		// Class: Yahoo
+		// Function: Sign an Oauth Request Designed for Yahoo BOSS API
+		// Yahoo BOSS: https://developer.yahoo.com/boss/
+		
+			// NOTE: YAHOO BOSS is deprecated!  You will not be able to query yahoo's services.
+			// This is presented as a demo of how to SIGN an Oauth Request.
+			
+			// This is offered under the BSD 2-clause license.
+	
+	class Yahoo
+	{
+		public function __construct($args)
+		{
+				# Actual lengths here are comparable
+			$this->client_id = 'INSERT-CLIENT-ID-HERE....................................................................';
+			$this->client_secret = 'INSERT-CLIENT-SECRET-HERE.........';
+		}
+		
+		public function getImageSearchUrl($args)
+		{
+			$oauth_version = '1.0';
+			$oauth_timestamp = time();
+			$oauth_nonce = hash('sha256', $oauth_timestamp);
+			$oauth_consumer_key = $this->client_id;
+			$oauth_signature_method = 'HMAC-SHA1';
+			$oauth_method = 'GET';
+			
+			$search_term = $args['searchterm'];
+			
+			$parameter_string = '';
+			$parameter_string .= 'oauth_consumer_key=' . $oauth_consumer_key;
+			$parameter_string .= '&oauth_nonce=' . $oauth_nonce;
+			$parameter_string .= '&oauth_signature_method=' . $oauth_signature_method;
+			$parameter_string .= '&oauth_timestamp=' . $oauth_timestamp;
+			$parameter_string .= '&oauth_version=' . $oauth_version;
+			$parameter_string .= '&q=' . $search_term;
+			
+			$yahoo_image_search_url = 'https://yboss.yahooapis.com/ysearch/images';		# ending '/' unnecessary
+			
+			$payload = $oauth_method . '&' . urlencode($yahoo_image_search_url) . '&' . urlencode($parameter_string);
+			$signature_key = $this->client_secret . '&';		# client secret + '&' + value of 'token' parameter
+			$oauth_signature = base64_encode(hash_hmac('sha1', $payload, $signature_key, TRUE));
+			
+			$full_url = $yahoo_image_search_url . '?' . $parameter_string;
+			$queryable_full_url = $full_url . '&oauth_signature=' . urlencode($oauth_signature);	
+			
+			return $queryable_full_url;
+		}
+	}
+	
+	$yahoo = new Yahoo();
+	$url = $yahoo->getImageSearchURL({'searchterm'=>'nature'});
+	
+		# GET URL for Retrieving JSON
+	print('URL? ' . $url . '<BR><BR>');
+	
+		# The Retrieved JSON Itself
+	print('Raw JSON? ' . file_get_contents($url));
+
+?>
